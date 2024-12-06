4c9d4b1045b399f5d972c64e71a18b5c1882baa0
karisazihni <trueekzl@gmail.com>
2024-12-06 14:04:26 +0700

    add template: user and detail

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('detail', views.detail, name='detail'),
+    path('login', views.login, name='login'),
+    path('regist', views.regist, name='regist'),
+    path('verif', views.verif, name='verif'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def detail(request):
+    return render(request, 'detail.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def regist(request):
+    return render(request, 'register.html')
+
+def verif(request):
+    return render(request, 'registration_success.html')
